41d3e21ee5edd8c05190187b7c5b690e12af7734
aboieriu <aboieriu@pentalog.fr>
2015-10-24 12:07:25 +0300

    Prepare for dev

--- app/js/modules/artist/router.js
+++
@@ -1,8 +0,0 @@
-var router = router || {};
-router.ArtistRouter = Marionette.AppRouter.extend({
-    controller: controller.artistController,
-    // "someMethod" must exist at controller.someMethod
-    appRoutes: {
-        "artist/:id": "showArtist"
-    }
-});
--- app/js/modules/home/router.js
+++
@@ -1,9 +0,0 @@
-var router = router || {};
-router.MainRouter = Marionette.AppRouter.extend({
-    controller: controller.mainController,
-    // "someMethod" must exist at controller.someMethod
-    appRoutes: {
-        "": "showHome",
-        "home": "showHome"
-    }
-});
